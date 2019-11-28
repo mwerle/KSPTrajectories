e80fcabd824e5446f3a3fab7c62bfe1f0bf2e41f
Micha Werle <Micha.Werle@sony.com>
2019-11-28 16:56:23 +0900

    Remove support for the bootstrap loader; older versions of KSP can no longer be supported due to the changed Unity version.

--- buildscripts/ZipPack/build-package.bat
+++ buildscripts/ZipPack/build-package.bat
@@ -14,21 +14,9 @@ SET scriptPath=%~dp0
 SET rootPath=%scriptPath%..\..\
 SET initialWD=%CD%
 
-echo Generating %TargetName% Bootstrap files...
 cd "%rootPath%"
-rem copy Bootstrap dll from build directory to GameData
-IF EXIST "%initialWD%\%TargetName%Bootstrap.dll" xcopy /y "%initialWD%\%TargetName%Bootstrap.dll" "GameData\%TargetName%\Plugin\*" > nul
-rem rename dll to KSP version specific bin
-echo %TargetName%.dll -^> %TargetName%%KSPversion%.bin
-move /y "%initialWD%\%TargetName%.dll" "%initialWD%\%TargetName%%KSPversion%.bin" > nul
-rem if built version is greater than KSP 1.3.1 then we assume latest version and copy built bin for the other compatible versions
-IF %KSPversion% GTR 13 xcopy /y "%initialWD%\%TargetName%%KSPversion%.bin" "%initialWD%\%TargetName%14.bin*" > nul
-IF %KSPversion% GTR 13 xcopy /y "%initialWD%\%TargetName%%KSPversion%.bin" "%initialWD%\%TargetName%15.bin*" > nul
-IF %KSPversion% GTR 13 xcopy /y "%initialWD%\%TargetName%%KSPversion%.bin" "%initialWD%\%TargetName%16.bin*" > nul
-rem delete Trajectories.bin if it exists
-IF EXIST "%initialWD%\%TargetName%.bin" del "%initialWD%\%TargetName%.bin"
-rem copy Bootstrap bins from build directory to GameData
-xcopy /y "%initialWD%\%TargetName%*.bin" "GameData\%TargetName%\Plugin\*" > nul
+rem copy Trajectories dll from build directory to GameData
+IF EXIST "%initialWD%\%TargetName%.dll" xcopy /y "%initialWD%\%TargetName%.dll" "GameData\%TargetName%\Plugin\*" > nul
 
 echo Generating %TargetName% Release Package...
 IF EXIST package\ rd /s /q package
@@ -54,8 +42,7 @@ IF EXIST "%rootPath%%TargetName%*.zip" del "%rootPath%%TargetName%*.zip"
 
 rem check all bootstrap files exist
 cd "%rootPath%"
-IF NOT EXIST "package\GameData\%TargetName%\Plugin\%TargetName%Bootstrap.dll" echo **WARNING** %TargetName%Bootstrap.dll is missing
-IF NOT EXIST "package\GameData\%TargetName%\Plugin\%TargetName%*.bin" echo **WARNING** %TargetName% bin is missing
+IF NOT EXIST "package\GameData\%TargetName%\Plugin\%TargetName%*.dll" echo **WARNING** %TargetName% dll is missing
 
 rem remove temp files
 rd /s /q package
